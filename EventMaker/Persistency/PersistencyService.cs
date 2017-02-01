eee9940b9590bdeb25ea8fbfcfb63b68ed23503a
Magnus <magnusemortensen@gmail.com>
2017-02-01 11:41:28 +0100

    LoadEventsFromAsync() SUH UDDEDE

--- EventMaker/Persistency/PersistencyService.cs
+++ EventMaker/Persistency/PersistencyService.cs
@@ -6,22 +6,36 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Windows.Storage;
 
 namespace EventMaker.Persistency
 {
     class PersistencyService
     {
+
+        static private readonly string filename = "EventFile.json";
+        static StorageFolder localfolder = null;
         public static async void SaveEventsAsJasonAsync(ObservableCollection<Event> events)
         {
-
+            string jsontext = JsonConvert.SerializeObject(events);
+            SerializeEventsFileAsync(jsontext, filename);
         }
 
 
         public static async Task<List<Event>> LoadEventsFromJsonAsync()
         {
+            Model.EventCatalogSingleton.EventCatalogSingletonInstance.EventListe.Clear();
+            Task<string> jsonTaskText = DeSerializeEventsFileAsync(filename);
 
+            List<Event> newList = JsonConvert.DeserializeObject<List<Event>>(jsonTaskText.ToString());
+            foreach (var i in newList)
+            {
+                newList.Add(i);
+            }
+            return newList;
         }
+
+
 
 
         public static async void SerializeEventsFileAsync(string eventsString, string fileName)
@@ -32,7 +46,11 @@ namespace EventMaker.Persistency
 
         public static async Task<string> DeSerializeEventsFileAsync(String fileName)
         {
-
+            List<Event> nyListe = JsonConvert.DeserializeObject<List<Event>>(fileName);
+            foreach (var e in nyListe)
+            {
+                EventCatalogSingleton.EventCatalogSingletonInstance.EventListe.Add(e);
+            }
         }
     }
 }
