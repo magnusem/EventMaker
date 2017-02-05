92f45b2716c8ff6af75cf001be30c91acabe8aaf
Magnus <Magnus@LAPTOP-96OVH7JD>
2017-02-05 12:39:53 +0100

    Persistency

--- EventMaker/Persistency/PersistencyService.cs
+++ EventMaker/Persistency/PersistencyService.cs
@@ -10,44 +10,49 @@ using Windows.Storage;
 
 namespace EventMaker.Persistency
 {
-    class PersistencyService
-    {
-        static private readonly string filename = "EventFile.json";
-        static StorageFolder localfolder = null;
+    #region
+    //    class PersistencyService
+    //    {
+    //        static private readonly string filename = "EventFile.json";
+    //        static StorageFolder localfolder = null;
 
-        public static async void SaveEventAsJsonAsync(ObservableCollection<Event> events)
-        {
-            string jsonText = JsonConvert.SerializeObject(events);
-            SerializeEventsFileAsync(jsonText, filename);
-        }
+    //        public static async void SaveEventAsJsonAsync(ObservableCollection<Event> events)
+    //        {
+    //            string jsonText = JsonConvert.SerializeObject(events);
+    //            SerializeEventsFileAsync(jsonText, filename);
+    //        }
 
-        public static async Task<List<Event>> LoadEventsFromJsonAsync()
-        {
-            Model.EventCatalogSingleton.EventCatalogSingletonInstance.EventListe.Clear();
-            Task<string> jsonTaskText = DeSerializeEventsFileAsync(filename);
-            List<Event> newList = JsonConvert.DeserializeObject<List<Event>>(jsonTaskText.ToString());
-            foreach (var i in newList)
-            {
-                newList.Add(i);
-            }
-            //Model.EventCatalogSingleton.Instance.EventListe
-            return newList;
-        }
+    //        public static async Task<List<Event>> LoadEventsFromJsonAsync()
+    //        {
+    //            Model.EventCatalogSingleton.EventCatalogSingletonInstance.EventListe.Clear();
 
-
-        private static async void SerializeEventsFileAsync(string eventsString, string filename)
-        {
-            StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
-            await FileIO.WriteTextAsync(file, eventsString);
-        }
+    //            Task<string> jsonTaskText = DeSerializeEventsFileAsync(filename);
+    //            List<Event> newList = JsonConvert.DeserializeObject<List<Event>>(jsonTaskText.ToString());
+    //            foreach (var i in newList)
+    //            {
+    //                newList.Add(i);
+    //            }
+    //            //Model.EventCatalogSingleton.Instance.EventListe
+    //            return newList;
+    //        }
 
 
-        private static async Task<string> DeSerializeEventsFileAsync(string filename)
-        {
-            StorageFile file = await localfolder.GetFileAsync(filename);
-            string jsonText = await FileIO.ReadTextAsync(file);
-            return jsonText;
-        }
-    }
+    //        private static async void SerializeEventsFileAsync(string eventsString, string filename)
+    //        {
+    //            StorageFile file = await localfolder.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+    //            await FileIO.WriteTextAsync(file, eventsString);
+    //        }
+
+
+    //        private static async Task<string> DeSerializeEventsFileAsync(string filename)
+    //        {
+    //            StorageFile file = await localfolder.GetFileAsync(filename);
+    //            string jsonText = await FileIO.ReadTextAsync(file);
+    //            return jsonText;
+    //        }
+    //    }
+    #endregion
+
+
 
 }
